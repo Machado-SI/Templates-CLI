f0d97a11607d138151eae6a3e3021cc5d59b3779
Machado-SI <223562434+Machado-SI@users.noreply.github.com>
2025-09-17 18:18:15 -0300

    feat: adicionar comando create com nome de projeto e opção de template

---
+++ bin/cli.js
@@ -0,0 +1,19 @@
+const { Command } = require('commander');
+const fs = require('fs-extra')
+const path = require('path')
+
+const progam = new Command();
+
+progam
+    .name('meu-cli')
+    .description('Meu CLI para criar templates de projetos')
+    .version('1.0.0')
+    .command('create')
+    .description('Cria um novo projeto')
+    .argument('<nome>', 'nome do projeto')
+    .option('-t', '--type <tipo>', 'fullstack ou backend', 'backend')
+    .action((nome, options) => {
+        
+    })
+
+progam.parse()
